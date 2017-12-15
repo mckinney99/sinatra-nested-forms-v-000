03a6cebbe6a812b8a25ba82db676e3c960ebb58d
Eric McKinney <mckinney99@gmail.com>
2017-12-15 18:58:11 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,22 +1,21 @@
 class Ship
-
-  attr_accessor :name, :type, :booty
-
-  SHIPS = []
-
-  def initialize(input)
-    @name = input[:name]
-    @type = input[:type]
-    @booty = input[:booty]
-    ships << self
+    attr_accessor :name, :type, :booty
+   
+    SHIPS = []
+    
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:type]
+        @booty = params[:booty]
+        SHIPS << self       
+    end
+    
+    def self.all
+        SHIPS
+    end
+    
+    def self.clear
+        SHIPS.clear
+    end
   end
-
-  def self.all
-    SHIPS
-  end
-
-  def self.clear
-    SHIPS.clear
-  end
-
-end
+end 
